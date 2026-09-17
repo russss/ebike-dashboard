53ff92675ec84cac25189d5bba5690cf4b0a3d25
Russ Garrett <russ@garrett.co.uk>
2026-09-17 17:28:29 +0100

    PWA stuff

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
@@ -7,5 +8,40 @@ export default defineConfig({
     environment: "jsdom",
     include: ["src/**/*.test.ts"],
   },
-  plugins: [basicSsl()],
+  plugins: [
+    basicSsl(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "E-bike Dashboard",
+        short_name: "E-bike Dashboard",
+        description: "E-bike Dashboard",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
